0f1dd07524fdcb701fc536ab2d1dbb4953cb2587
Benjamin <bancel@agilepartner.net>
2023-11-10 15:03:37 +0100

    Delete obsele test because no exception can be thrown anymore

--- typescript/src/data-types/Plays.ts
+++ typescript/src/data-types/Plays.ts
@@ -6,3 +6,29 @@ export type Plays = {
 };
 
 export type PlayType = "tragedy" | "comedy";
+
+export type PlayMap = Record<PlayType, (audience: number) => number>;
+
+export function calculateAmount(): PlayMap {
+  return {
+    comedy: calculateComedyAmount,
+    tragedy: calculateTragedyAmount
+  }
+}
+
+function calculateComedyAmount(audience: number): number {
+  let thisAmount = 30000;
+  if (audience > 20) {
+    thisAmount += 10000 + 500 * (audience - 20);
+  }
+  thisAmount += 300 * audience;
+  return thisAmount;
+}
+
+function calculateTragedyAmount(audience: number): number {
+  let thisAmount = 40000;
+  if (audience > 30) {
+    thisAmount += 1000 * (audience - 30);
+  }
+  return thisAmount;
+}
--- typescript/src/statement.ts
+++ typescript/src/statement.ts
@@ -1,6 +1,6 @@
 import { Invoice } from "./data-types/Invoice";
 import { Performance } from "./data-types/Performance";
-import { Plays, PlayType } from "./data-types/Plays";
+import { Plays, PlayType, calculateAmount } from "./data-types/Plays";
 
 function statement(invoice: Invoice, plays: Plays): string {
   let totalAmount = 0;
@@ -14,11 +14,14 @@ function statement(invoice: Invoice, plays: Plays): string {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = calculatePlayAmount(play.type, perf.audience);
+    let thisAmount = calculateAmount()[play.type](perf.audience);
+
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
+
     // add extra credit for every ten comedy attendees
     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
     // print line for this order
     result += ` ${play.name}: ${format(thisAmount / 100)} (${
       perf.audience
@@ -28,34 +31,6 @@ function statement(invoice: Invoice, plays: Plays): string {
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
-}
-
-function calculatePlayAmount(playType: PlayType, audience: number): number {
-  switch (playType) {
-    case "tragedy":
-      return calculateTragedyAmount(audience);
-    case "comedy":
-      return calculateComedyAmount(audience);
-    default:
-      throw new Error(`unknown type: ${playType}`);
-  }
-}
-
-function calculateComedyAmount(audience: number): number {
-  let thisAmount = 30000;
-  if (audience > 20) {
-    thisAmount += 10000 + 500 * (audience - 20);
-  }
-  thisAmount += 300 * audience;
-  return thisAmount;
-}
-
-function calculateTragedyAmount(audience: number): number {
-  let thisAmount = 40000;
-  if (audience > 30) {
-    thisAmount += 1000 * (audience - 30);
-  }
-  return thisAmount;
 }
 
 export { statement };
