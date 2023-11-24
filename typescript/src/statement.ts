c85e8bf7562c3a344f0d12813b0249a9f02c9f2b
Florian SIERADZKI <fsieradzki@agilepartner.net>
2023-11-24 14:46:42 +0100

    factorize final output

--- typescript/src/statement.ts
+++ typescript/src/statement.ts
@@ -9,34 +9,45 @@ import {
 import { formattedAmount } from "./data-types/format";
 
 function statement(invoice: Invoice, plays: Plays): string {
-  function toTotal(performance: Performance, plays: Plays) {
-    const play = plays[performance.playID];
-    const amount = calculateAmount()[play.type](performance.audience);
-    return {
-      amount: amount,
-      credit: calculatePlayCredit()[play.type](performance.audience),
-      resultString: ` ${play.name}: ${formattedAmount(amount)} (${
-        performance.audience
-      } seats)\n`,
-    };
-  }
-
   let resultAndTotalAmount = invoice.performances
-    .map((item) => toTotal(item, plays))
+    .map((item) => getPerformanceData(item, plays))
     .reduce(
       (acc, item) =>
         (acc = {
           amount: acc.amount + item.amount,
           credit: acc.credit + item.credit,
-          resultString: acc.resultString + item.resultString,
+          result: acc.result + item.result,
         })
     );
+  return getOutput(invoice.customer, resultAndTotalAmount);
+}
+type PerformanceData = {
+  result: string;
+  amount: number;
+  credit: number;
+};
 
-  let result = `Statement for ${invoice.customer}\n`;
-  result += resultAndTotalAmount.resultString;
-  result += `Amount owed is ${formattedAmount(resultAndTotalAmount.amount)}\n`;
-  result += `You earned ${resultAndTotalAmount.credit} credits\n`;
+function getOutput(customer: string, performanceData: PerformanceData): string {
+  let result = `Statement for ${customer}\n`;
+  result += performanceData.result;
+  result += `Amount owed is ${formattedAmount(performanceData.amount)}\n`;
+  result += `You earned ${performanceData.credit} credits\n`;
   return result;
+}
+
+function getPerformanceData(
+  performance: Performance,
+  plays: Plays
+): PerformanceData {
+  const play = plays[performance.playID];
+  const amount = calculateAmount()[play.type](performance.audience);
+  return {
+    amount: amount,
+    credit: calculatePlayCredit()[play.type](performance.audience),
+    result: ` ${play.name}: ${formattedAmount(amount)} (${
+      performance.audience
+    } seats)\n`,
+  };
 }
 
 export { statement };
