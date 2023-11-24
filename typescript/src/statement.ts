ec09ef5e4d38df8d92ce39b85cf1957d06237a8f
Florian SIERADZKI <fsieradzki@agilepartner.net>
2023-11-24 14:33:45 +0100

    replace foreach by map reduce

--- typescript/src/statement.ts
+++ typescript/src/statement.ts
@@ -1,34 +1,47 @@
 import { Invoice } from "./data-types/Invoice";
 import { Performance } from "./data-types/Performance";
-import { Plays, PlayType, calculateAmount, calculatePlayCredit } from "./data-types/Plays";
+import {
+  Plays,
+  PlayType,
+  calculateAmount,
+  calculatePlayCredit,
+} from "./data-types/Plays";
 
 function statement(invoice: Invoice, plays: Plays): string {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
   const format = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2,
   }).format;
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    const amount = calculateAmount()[play.type](perf.audience);
-
-    volumeCredits += calculatePlayCredit()[play.type](perf.audience);;
-
-    // print line for this order
-    result += ` ${play.name}: ${format(amount / 100)} (${
-      perf.audience
-    } seats)\n`;
-    totalAmount += amount;
+  function toTotal(performance: Performance, plays: Plays) {
+    const play = plays[performance.playID];
+    const amount = calculateAmount()[play.type](performance.audience);
+    return {
+      amount: amount,
+      credit: calculatePlayCredit()[play.type](performance.audience),
+      resultString: ` ${play.name}: ${format(amount / 100)} (${
+        performance.audience
+      } seats)\n`,
+    };
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+
+  let resultAndTotalAmount = invoice.performances
+    .map((item) => toTotal(item, plays))
+    .reduce(
+      (acc, item) =>
+        (acc = {
+          amount: acc.amount + item.amount,
+          credit: acc.credit + item.credit,
+          resultString: acc.resultString + item.resultString,
+        })
+    );
+
+  let result = `Statement for ${invoice.customer}\n`;
+  result += resultAndTotalAmount.resultString;
+  result += `Amount owed is ${format(resultAndTotalAmount.amount / 100)}\n`;
+  result += `You earned ${resultAndTotalAmount.credit} credits\n`;
   return result;
 }
 
 export { statement };
-
-
